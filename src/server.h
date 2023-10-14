3fe5d6e2d9f1cf0cf11d0dbfbc2aec40a9d6b38b
piotrselak <piotr.selak.it@gmail.com>
2023-10-14 17:58:37 +0200

    small fix

--- src/server.h
+++ src/server.h
@@ -4,6 +4,7 @@
 #include <boost/asio/impl/io_context.ipp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/bind.hpp>
+#include <boost/asio/placeholders.hpp>
 #include "tcp-connection.h"
 
 using boost::asio::ip::tcp;
@@ -12,20 +13,19 @@ class tcp_server {
 public:
     explicit tcp_server(boost::asio::io_context &io_context)
             : io_context_(io_context),
-              acceptor_(io_context, tcp::endpoint(tcp::v4(), 10000)) {
+              acceptor_(io_context, tcp::endpoint(tcp::v4(), 10000)) { // TODO: Change to be configurable
         start_accept();
     }
 
 private:
     void start_accept() {
-        tcp_connection::pointer new_connection =
-                tcp_connection::create(io_context_);
+        tcp_connection::pointer new_connection = tcp_connection::create(io_context_);
 
         acceptor_.async_accept(new_connection->socket(),
                                boost::bind(&tcp_server::handle_accept,
                                            this,
                                            new_connection,
-                                           boost::placeholders::_1)); // TODO: This _1 may create error
+                                           boost::asio::placeholders::error));
     }
 
     void handle_accept(tcp_connection::pointer new_connection, const boost::system::error_code &error) {
