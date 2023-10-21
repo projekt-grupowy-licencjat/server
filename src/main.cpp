13fefb73b4a23c954cb21ffb2473280c077cbf7e
piotrselak <piotr.selak.it@gmail.com>
2023-10-22 01:43:45 +0200

    Added todos

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,11 @@
 using boost::asio::ip::tcp;
 
 int main() {
+    // TODO: first load all of equipment etc data to memory
+    // TODO: Maybe change everything to UDP? idk if tcp is even needed here
+    // TODO: Save all the data to files every minute or so
+    // TODO: Send players location and movement direction eg 32 times per second
+    // TODO: Send equipment and other stuff when it changes
     try {
         boost::asio::io_context io_context;
         tcp_server server(io_context);
